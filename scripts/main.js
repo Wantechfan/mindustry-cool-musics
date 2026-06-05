3177e476deef720fc6607eb9da3653463b4a388b
WantechFan <alifiandratsaqifp@gmail.com>
2026-06-05 14:30:06 +0700

    Pls bro

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-const cool-musics = Vars.mods.locateMod("cool-musics").meta;
+const coolmusics = Vars.mods.locateMod("cool-musics").meta;
 
-cool-musics.displayName = Core.bundle.get("mod.cool-musics.name");
-cool-musics.author = Core.bundle.get("mod.cool-musics.author");
-cool-musics.description = Core.bundle.get("mod.cool-musics.description");
-cool-musics.subtitle = Core.bundle.get("mod.cool-musics.subtitle");
+coolmusics.displayName = Core.bundle.get("mod.cool-musics.name");
+coolmusics.author = Core.bundle.get("mod.cool-musics.author");
+coolmusics.description = Core.bundle.get("mod.cool-musics.description");
+coolmusics.subtitle = Core.bundle.get("mod.cool-musics.subtitle");
