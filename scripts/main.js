b72315d27c54b72d3e3c23b642b7171d03aa05ed
WantechFan <alifiandratsaqifp@gmail.com>
2026-06-06 13:26:32 +0700

    Scrapped

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-const coolmusics = Vars.mods.locateMod("cool-musics").meta;
-
-coolmusics.displayName = Core.bundle.get("mod.cool-musics.name");
-coolmusics.author = Core.bundle.get("mod.cool-musics.author");
-coolmusics.description = Core.bundle.get("mod.cool-musics.description");
-coolmusics.subtitle = Core.bundle.get("mod.cool-musics.subtitle");
